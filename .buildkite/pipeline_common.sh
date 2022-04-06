04baff47add0950f6a7884a99c8a0f6cd97d5a65
Igor Gnatyuk <igorgn@wix.com>
2022-04-06 15:47:05 +0300

    check creds

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -7,9 +7,9 @@
 # [[ $PR_REPO != '' ]] && [[ $ORIGINAL_REPO != $PR_REPO ]] && echo '  - block: ":rocket: Release!"'
 
 
-cat .buildkite/jobs/pipeline.ios_rn_64.yml
+# cat .buildkite/jobs/pipeline.ios_rn_64.yml
 # cat .buildkite/jobs/pipeline.ios_rn_67.yml
-# cat .buildkite/jobs/pipeline.android_rn_64.yml
+cat .buildkite/jobs/pipeline.android_rn_64.yml
 # cat .buildkite/jobs/pipeline.android_rn_67.yml
 # cat .buildkite/jobs/pipeline.android_demo_app_rn_67.yml
 # cat .buildkite/jobs/pipeline.ios_demo_app_rn_67.yml
